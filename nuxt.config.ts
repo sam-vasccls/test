d680e3274e8ab4a9b783cc62633ea0e38d1144e8
Sam Vasconcelos <svasconcelos@bonadmin.com>
2024-11-30 20:29:35 -0300

    :wrench: update nuxt.config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
     devtools: { enabled: true },
     css: ["~/assets/scss/main.scss"],
-    modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon', "@nuxt/icon"],
+    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icon", "@nuxt/icon"],
     vite: {
         css: {
             preprocessorOptions: {
                 scss: {
-                    additionalData: '@import "@/assets/scss/variables.scss";'
-                }
-            }
-        }
-    }
-})
+                    additionalData: '@import "@/assets/scss/variables.scss";',
+                },
+            },
+        },
+    },
+});
